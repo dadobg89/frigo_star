74e84d88f6f313781e9cab3e6d6882af2a21ddd3
misha85 <milosh.djurdjevic@gmail.com>
2017-04-29 11:56:16 +0200

    implementiranje osnovne veryije slajdera

--- slider.php
+++ slider.php
@@ -1,6 +1,8 @@
 <?php
 require 'include/head.html';
-switch ($_GET['elements']) { // switch sluzi za generisanje naslova kao i za putanje za prikazivanje slika u slajderu
+$images = array(1,2,3,4,5,6,7);
+
+switch ($_GET['elements']) { // switch sluzi za generisanje naslova
 	case 'termički':
 		$naslov = "Termički uređaji";
 		break;
@@ -21,14 +23,44 @@ switch ($_GET['elements']) { // switch sluzi za generisanje naslova kao i za put
 		# code...
 		break;
 }
+
 ?>
 
+
+
+
+    <!-- jQuery library (served from Google) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <!-- bxSlider Javascript file -->
+    <script src="jquery.bxslider/jquery.bxslider.js"></script>
+    <!-- bxSlider CSS file -->
+    <link href="jquery.bxslider/jquery.bxslider.css" rel="stylesheet" />
+
+
 <div class="wrapper">
-	<h2 class="slider_h2"><?php echo $naslov ?></h2>
 	<article class="slider">
-		
+		<h2 class="slider_h2"><?php echo $naslov ?></h2>
+		<ul class="bxslider">
+		  <?php 
+		  foreach ($images as $image) {
+		  	 echo "<li><img src='images/".$_GET['elements']."/".$image.".jpg'></li>";
+		  }
+		   ?>
+		</ul>
 	</article>
+
+
+
+
+	<section id="drugi">
+		<a href="slider.php?elements=termički" class="dugme">termički urađaji</a>
+		<a href="slider.php?elements=rashladni" class="dugme">rashladni urađaji</a>
+		<a href="slider.php?elements=neutralna" class="dugme">neutralna oprema</a>
+		<a href="slider.php?elements=termika" class="dugme">termika</a>
+	</section>
 </div>
+
+
 
 
 </body>
